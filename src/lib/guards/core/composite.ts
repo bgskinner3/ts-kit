95c30e7edde32623962a4bb279bfdd99778ae663
bgskinner3 <bgskinner3@gmail.com>
2026-03-14 03:25:37 -0400

    resolved

--- src/lib/guards/core/composite.ts
+++ src/lib/guards/core/composite.ts
@@ -1,5 +1,5 @@
 import type { TTypeGuard } from '../../../types';
-import { isObject, isUndefined, isDefined } from './reference';
+import { isObject, isDefined } from './reference';
 import { isBoolean, isNumber, isSymbol, isString } from './primitives';
 import { ObjectUtils } from '../../common/object';
 
